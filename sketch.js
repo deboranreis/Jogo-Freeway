5bb14610c06b7344a6aa23b730b67b91dc692999
deboranreis <delnayara_@hotmail.com>
2023-02-24 16:15:50 -0300

    Adicionando arquivos de configurações das funções

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(600, 400);
+    somDaTrilha.loop();
+  }
+  
+  function draw() {
+    background(imagemDaEstrada);
+    mostraAtor();
+    mostraCarro();
+    movimentaCarro();
+    movimentaAtor(); 
+    voltaPosicaoInicialDoCarro();
+    voltaPosicaoInicialAtor();
+    verificaColisao();
+    incluiPontos();
+    marcaPontos();
+    pontosMaiorQueZero();
+  }
